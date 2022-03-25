7d482fa67acaff50d59bb4b973608fbd00a0c634
Ahmed R. Alsaber <39394494+alsaberACS@users.noreply.github.com>
2022-03-25 13:39:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ def main():
             
     new_title1 = '<p style="font-family:sans-serif; color:Green; font-size: 20px;">Prepared By : </p>'
     st.markdown(new_title1, unsafe_allow_html=True)
-    new_title2 = '<p style="font-family:sans-serif; color:Blue; font-size: 15px;">➡ Ahmed R.Alsaber </p>'
+    new_title2 = '<p style="font-family:sans-serif; color:Blue; font-size: 15px;">➡ Dr. Ahmed R.Alsaber </p>'
     st.markdown(new_title2, unsafe_allow_html=True)
-    new_title3 = '<p style="font-family:sans-serif; color:purple; font-size: 14px;">   Lecturer at College of Business And Economics </p>'
+    new_title3 = '<p style="font-family:sans-serif; color:purple; font-size: 14px;">   Lecturer at College of Business And Economics (CBE) </p>'
     st.markdown(new_title3, unsafe_allow_html=True)
     new_title4 = '<p style="font-family:sans-serif; color:purple; font-size: 14px;"> American University of Kuwait  <a  href=" mailto: aalsaber@auk.edu.kw"> Email </a> </p> '
     st.markdown(new_title4, unsafe_allow_html=True)
